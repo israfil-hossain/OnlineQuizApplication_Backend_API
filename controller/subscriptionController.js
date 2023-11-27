9b01c1f46bfb4e69dd5b817e2e4d00580b652761
Israfil-Digi360 <mailto:israfil.hossain@digigate360.com>
2023-11-27 13:18:04 +0600

    Update Subscription api

--- controller/subscriptionController.js
+++ controller/subscriptionController.js
@@ -11,7 +11,7 @@ const getSubscription = async (req, res, next) => {
       const firstSubscription = subscriptions[0];
       res.json(firstSubscription);
     } else {
-      res.json({ msg:"No Data Found !"}); // or you can send an appropriate response for an empty array
+      res.json({}); // or you can send an appropriate response for an empty array
     }
   } catch (err) {
     return res.status(500).json({
@@ -24,14 +24,10 @@ const getSubscription = async (req, res, next) => {
   }
 };
 
-const updateSubscription = async (req, res, next) => {
+
+async function updateSubscription(req, res, next) {
   const { id } = req.params;
   const { subscription } = req.body;
-
-  // Validate if id is a valid ObjectId
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: 'Invalid subscription ID' });
-  }
 
   try {
     const data = await Subscription.findByIdAndUpdate(
@@ -41,32 +37,28 @@ const updateSubscription = async (req, res, next) => {
     );
 
     if (!data) {
-      console.error(`Subscription with ID ${id} not found`);
       return res.status(404).json({ error: 'Subscription not found' });
     }
-
-    console.log(`Subscription with ID ${id} updated successfully`);
     res.status(200).json({
       success: true,
-      message: 'Subscription updated successfully!',
+      message: "Subscription Update Successfully !",
       data: subscription,
     });
+
+   
   } catch (err) {
-    console.error('Error updating subscription:', err);
+    console.log(err);
     return res.status(500).json({
       success: false,
-      message: 'Unknown error occurred!',
+      message: "Unknown error occurred!",
       errors: {
         common: {
-          msg: 'Unknown error occurred!',
-          error: err.message
+          msg: "Unknown error occurred!",
         },
       },
     });
   }
-};
-
-
+}
 
 module.exports = {
     getSubscription,
